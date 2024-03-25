f41b04230aa06f0c6c8260d9151c0aa8ca02338c
pbckman <143405511+pbckman@users.noreply.github.com>
2024-03-25 19:05:56 +0100

    functionality for sign out added to header + newsletter

--- Silicon_1/Controllers/HomeController.cs
+++ Silicon_1/Controllers/HomeController.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Silicon_1.Models;
 
 namespace Silicon_1.Controllers;
 
-public class HomeController : Controller
+public class HomeController(DataContext dataContext) : Controller
 {
+    private readonly DataContext _dataContext = dataContext;
+
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Subscribe(SubscribeViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var subscriberEntity = new SubscriberEntity
+            {
+                Email = model.Email,
+                DailyNewsletter = model.DailyNewsletter,
+                AdvertisingUpdates = model.AdvertisingUpdates,
+                WeekinReview = model.WeekinReview,
+                EventUpdates = model.EventUpdates,
+                StartupsWeekly = model.StartupsWeekly,
+                Podcasts = model.Podcasts,
+            };
+
+            _dataContext.Subscribers.Add(subscriberEntity);
+            await _dataContext.SaveChangesAsync();
+
+        }
+
+        return RedirectToAction("Index", "Home");
     }
 }
--- Silicon_1/obj/Debug/net8.0/Silicon_1.AssemblyInfo.cs
+++ Silicon_1/obj/Debug/net8.0/Silicon_1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a4a90413a96e576a9cb7114b025f9b6ae14be97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247d219dfb2697b92208278746f79b8f07438c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
