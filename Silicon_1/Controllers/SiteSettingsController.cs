c9961582d7d7c9503e20a95bbd828f01c6cc37e6
pbckman <143405511+pbckman@users.noreply.github.com>
2024-04-03 17:06:16 +0200

    darkmode + cookie consent added

---
+++ Silicon_1/Controllers/SiteSettingsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon_1.Controllers;
+
+public class SiteSettingsController : Controller
+{
+    public IActionResult Theme(string mode)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(180)
+        };
+        Response.Cookies.Append("theme", mode, option);
+        return Ok();
+    }
+
+
+    public IActionResult Consent(string mode)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddYears(1),
+            HttpOnly = true,
+            Secure = true
+        };
+        Response.Cookies.Append("consent", "true", option);
+        return Ok();
+    }
+}
--- Silicon_1/wwwroot/js/site.js
+++ Silicon_1/wwwroot/js/site.js
@@ -1,1 +1,28 @@
 ﻿
+document.addEventListener('DOMContentLoaded', function () {
+
+    darkModeSwitch()
+
+})
+
+
+function darkModeSwitch() {
+
+    try {
+
+        let sw = document.querySelector('#switch-mode')
+        sw.addEventListener('change', function () {
+            let mode = this.checked ? 'dark' : 'light'
+
+            fetch(`/sitesettings/theme?mode=${mode}`)
+                .then(res => {
+                    if (res.ok)
+                        window.location.reload()
+                    else
+                        console.log('unable to switch theme mode')
+                })
+        })
+
+    }
+    catch { }
+}
