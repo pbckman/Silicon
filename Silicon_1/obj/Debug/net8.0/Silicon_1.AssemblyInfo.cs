ba369ee6d2b5298126ccd2ef89f9cbdefe33081c
pbckman <143405511+pbckman@users.noreply.github.com>
2024-04-13 22:23:45 +0200

    html for course details added + css base

--- Silicon_1/obj/Debug/net8.0/Silicon_1.AssemblyInfo.cs
+++ Silicon_1/obj/Debug/net8.0/Silicon_1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2af1019866d1b7f6c5b69bdee3cec0455052ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a582a5de2f1381370f609dad2357830f2bc7e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
