15fd6ad498c0dfc2b340af2a833c216be33b2b8f
pbckman <143405511+pbckman@users.noreply.github.com>
2024-04-19 12:54:19 +0200

    responsive css added to all other views except account details+security

--- Silicon_1/obj/Debug/net8.0/Silicon_1.AssemblyInfo.cs
+++ Silicon_1/obj/Debug/net8.0/Silicon_1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960d2f5e34c3ae4224a4232b445cd38388993e35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdae461a7b1d5358ad6210cc47c104b1cdc8ff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Silicon_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
